a261cf0919126f15cad0759ff96b70a333366464
Faizan <muhammadfaizank119@gmail.com>
2024-06-12 14:09:10 +0500

    fix deployment error

--- components/TranslationHistory.tsx
+++ components/TranslationHistory.tsx
@@ -1,78 +1,3 @@
-// import { ITranslation } from "@/mongodb/models/User";
-// import { auth } from "@clerk/nextjs/server";
-// import DeleteTranslationButton from "./DeleteTranslationButton";
-// // import TimeAgo from "react-timeago";
-// import TimeAgoText from "./TimeAgoText";
-
-// const getLanguage = (code: string) => {
-//   const lang = new Intl.DisplayNames(["en"], { type: "language" });
-//   return lang.of(code);
-// };
-
-// async function TranslationHistory() {
-//   const { userId } = auth();
-
-//   const url = `${
-//     process.env.NODE_ENV === "development"
-//       ? "http://localhost:3000"
-//       : process.env.VERCEL_URL
-//   }/translationHistory?userId=${userId}`;
-
-//   const response = await fetch(url, {
-//     next: {
-//       tags: ["translationHistory"],
-//     },
-//   });
-
-//   const { translations }: { translations: Array<ITranslation> } =
-//     await response?.json();
-//   console.log("*********translations", translations);
-
-//   return (
-//     <div className="">
-//       <h1 className="text-3xl my-5">History</h1>
-
-//       {/* Show a message if there are no translations */}
-//       {translations.length === 0 && (
-//         <p className="mb-5 text-gray-400">No translations yet</p>
-//       )}
-
-//       {/* Show a list of translations */}
-//       <ul className="divide-y border rounded-md">
-//         {translations?.map((translation) => (
-//           <li
-//             key={translation._id as string}
-//             className="flex justify-between items-center p-5 hover:bg-gray-50 relative"
-//           >
-//             <div>
-//               <p className="text-sm mb-5 text-gray-500">
-//                 {getLanguage(translation.from)}
-//                 {" -> "}
-//                 {getLanguage(translation.to)}
-//               </p>
-
-//               <div className="space-y-2 pr-5">
-//                 <p>{translation.fromText}</p>
-//                 <p className="text-gray-400">{translation.toText}</p>
-//               </div>
-//             </div>
-
-//             <p className="text-sm text-gray-300 absolute top-2 right-2">
-//               <TimeAgoText
-//                 date={new Date(translation.timestamp).toISOString()}
-//               />
-//             </p>
-
-//             <DeleteTranslationButton id={translation._id as string} />
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default TranslationHistory;
-
 import { ITranslation } from "@/mongodb/models/User";
 import { auth } from "@clerk/nextjs/server";
 import DeleteTranslationButton from "./DeleteTranslationButton";
@@ -85,22 +10,12 @@ const getLanguage = (code: string) => {
 
 async function TranslationHistory() {
   const { userId } = auth();
-  if (!userId) throw new Error("User not logged in");
 
-  let url;
-  try {
-    const baseUrl =
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : process.env.VERCEL_URL;
-
-    if (!baseUrl) throw new Error("Base URL is not defined");
-
-    url = new URL(`/translationHistory?userId=${userId}`, baseUrl).toString();
-  } catch (error) {
-    console.error("Failed to construct URL:", error);
-    throw new Error("Failed to construct URL");
-  }
+  const url = `${
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : process.env.VERCEL_URL
+  }/translationHistory?userId=${userId}`;
 
   const response = await fetch(url, {
     next: {
@@ -116,10 +31,12 @@ async function TranslationHistory() {
     <div className="">
       <h1 className="text-3xl my-5">History</h1>
 
+      {/* Show a message if there are no translations */}
       {translations.length === 0 && (
         <p className="mb-5 text-gray-400">No translations yet</p>
       )}
 
+      {/* Show a list of translations */}
       <ul className="divide-y border rounded-md">
         {translations?.map((translation) => (
           <li
